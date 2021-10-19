3fe530603225b1f503338017ac8078ba01e033cc
papagianEva <evapapag2000@gmail.com>
2021-10-19 21:24:29 +0300

    New json

--- extension/src/browser/extension-widget.tsx
+++ extension/src/browser/extension-widget.tsx
@@ -4,7 +4,7 @@ import { AlertMessage } from '@theia/core/lib/browser/widgets/alert-message';
 import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
 import { MessageService } from '@theia/core';
 //import { workspace} from '@theia/plugin';
-import { integer } from 'vscode-languageserver-types';
+import data from './data.json';
 
 
 
@@ -34,258 +34,7 @@ export class extensionWidget extends ReactWidget {
 	}
 	
 	static setState: any;
-	
-	static arrayOfJSON = [
-		{//???
-		   "name":"Abstract Factory",
-		   "values":[
-			  "AbstractFactory",
-			  "ConcreteFactory",
-			  "AbstractProduct1",
-			  "Product1",
-			  "Client"
-		   ],
-		   "btnExtension":[0,0,1,1,0],
-		   "counter": 1,
-		},
-		{
-		   "name":"Builder",
-		   "values":[
-			  "Director",
-			  "Builder",
-			  "ConcreteBuilder1",
-			  "Product1"
-		   ],
-		   "btnExtension":[0,0,1,1],
-		   "counter": 1,
-		},
-		{
-		   "name":"Factroy Method",
-		   "values":[
-			  "Product",
-			  "ConcreteProduct",
-			  "Creator",
-			  "ConcreteCreator"
-		   ],
-		   "btnExtension":[0,0,0,0],
-		   "counter": 1,
-		},
-		{
-		   "name":"Prototype",
-		   "values":[
-			  "Client",
-			  "Prototype",
-			  "ConcretePrototype1"
-		   ],
-		   "btnExtension":[0,0,1],
-		   "counter": 1,
-		},
-		{
-		   "name":"Singleton",
-		   "values":[
-			  "Singleton"
-		   ],
-		   "btnExtension":[0],
-		   "counter": 1,
-		},
-		{
-		   "name":"Adapter",
-		   "values":[
-			  "Client",
-			  "Target",
-			  "Adapter1",
-			  "Adaptee1"
-		   ],
-		   "btnExtension":[0,0,1,1],
-		   "counter": 1,
-		},
-		{
-		   "name":"Bridge",
-		   "values":[
-			  "Client",
-			  "Abstraction",
-			  "RefinedAbstraction1",
-			  "Implementor",
-			  "ConcreteImplementor1"
-		   ],
-		   "btnExtension":[0,0,1,0,1],
-		   "counter": 1,
-		},
-		{
-		   "name":"Composite",
-		   "values":[
-			  "Client",
-			  "Component",
-			  "Leaf1",
-			  "Composite1"
-		   ],
-		   "btnExtension":[0,0,1,1],
-		   "counter": 1,
-		},
-		{
-		   "name":"Decorator",
-		   "values":[
-			  "Component",
-			  "ConcreteComponent1",
-			  "Decorator",
-			  "ConcreteDecorator1"
-		   ],
-		   "btnExtension":[0,1,0,1],
-		   "counter": 1,
-		},
-		{//??
-		   "name":"Facade",
-		   "values":[
-			  ""
-		   ],
-		   "btnExtension":[0],
-		   "counter": 1,
-		},
-		{
-		   "name":"Flyweight",
-		   "values":[
-			  "Client",
-			  "FlyweightFactory",
-			  "Flyweight",
-			  "ConcreteFlyweight",
-			  "UnsharedConcreteFlyweight",
-			  "Client"
-		   ],
-		   "btnExtension":[0,0,0,0],
-		   "counter": 1,
-		},
-		{
-		   "name":"Proxy",
-		   "values":[
-			  "Client",
-			  "Subject",
-			  "Proxy",
-			  "RealSubject"
-		   ],
-		   "btnExtension":[0,0,0,0],
-		   "counter": 1,
-		},
-		{
-		   "name":"Chain of Responsibility",
-		   "values":[
-			  "Client",
-			  "Handler",
-			  "ConcreteHandler1"
-		   ],
-		   "btnExtension":[0,0,1],
-		   "counter": 1,
-		},
-		{
-		   "name":"Command",
-		   "values":[
-			  "Command",
-			  "ConcreteCommand1",
-			  "Client",
-			  "Invoker",
-			  "Receiver"
-		   ],
-		   "btnExtension":[0,1,0,0,0],
-		   "counter": 1,
-		},
-		{//???/
-		   "name":"Interpreter",
-		   "values":[
-			  "Client",
-			  "Context",
-			  "AbstractExpression",
-			  "TerminalExpression",
-			  "NonterminalExpression"
-		   ],
-		   "btnExtension":[0,0,0,0,0],
-		   "counter": 1,
-		},
-		{///???
-		   "name":"Iterator",
-		   "values":[
-			  "Aggregate",
-			  "Client",
-			  "Iterator",
-			  "ConcreteAggregate1",
-			  "ConcreteIterator1"
-		   ],
-		   "btnExtension":[0,0,0,1,1],
-		   "counter": 1,
-		},
-		{
-		   "name":"Mediator",
-		   "values":[
-			  "Mediator",
-			  "Colleague",
-			  "ConcreteMediator",
-			  "ConcreteColleague1"
-		   ],
-		   "btnExtension":[0,0,0,1],
-		   "counter": 1,
-		},
-		{///???
-		   "name":"Memento",
-		   "values":[
-			  "Originator",
-			  "Memento",
-			  "Caretaker"
-		   ],
-		   "btnExtension":[0,0,0],
-		   "counter": 1,
-		},
-		{//???/
-		   "name":"Observer",
-		   "values":[
-			  "Subject",
-			  "Observer",
-			  "ConcreteSubject",
-			  "ConcreteObserver"
-		   ],
-		   "btnExtension":[0,0,0,0],
-		   "counter": 1,
-		},
-		{
-		   "name":"State",
-		   "values":[
-			  "Context",
-			  "State",
-			  "ConcreteState1"
-		   ],
-		   "btnExtension":[0,0,1],
-		   "counter": 1,
-		},
-		{
-		   "name":"Strategy",
-		   "values":[
-			  "Context",
-			  "Strategy",
-			  "ConcreteStrategy1"
-		   ],
-		   "btnExtension":[0,0,1],
-		   "counter": 1,
-		},
-		{
-		   "name":"Template Method",
-		   "values":[
-			  "AbstractClass",
-			  "ConcreteClass"
-		   ],
-		   "btnExtension":[0,0],
-		   "counter": 1,
-		},
-		{
-		   "name":"Visitor",
-		   "values":[
-			  "Client",
-			  "Visitor",
-			  "ConcreteVisitor1",
-			  "ObjectStructure",
-			  "Element",
-			  "ConcreteElement1"
-		   ],
-		   "btnExtension":[0,0,1,0,0,1],
-		   "counter": 1,
-		}
-	 ];
+	static data = JSON.parse(JSON.stringify(data));
 	protected render(): React.ReactNode {
 		const header = `Choose a Design Pattern and get the code. `;
 		
@@ -338,11 +87,8 @@ export class extensionWidget extends ReactWidget {
 				<br /> 
 				<div id="result">
 					<table id="show_pattern_table">
-
 					</table>
-					
 				</div>
-				
 			</div>
 			</div>
 	}
@@ -350,38 +96,51 @@ export class extensionWidget extends ReactWidget {
     protected runprocess(): void {
 		if (extensionWidget.state.statePatternSelection!="Choose_pattern" && extensionWidget.state.statePatternSelection!="" && extensionWidget.state.stateURLproject!="" && extensionWidget.state.stateURLproject!='Project URL'){
 			(document.getElementById("btn-get-code") as HTMLButtonElement).style.visibility = 'hidden';
-			 
+			
 			//show the JSON values for the chosen key-pattern
-			var  index = extensionWidget.arrayOfJSON.findIndex(x => x.name === extensionWidget.state.statePatternSelection);
-			console.log(index);
-			var values = extensionWidget.arrayOfJSON[index]["values"];
-			var btnExtension = extensionWidget.arrayOfJSON[index]["btnExtension"] ;
-			var counter = extensionWidget.arrayOfJSON[index]["counter"] ;
+			var values = extensionWidget.data[extensionWidget.state.statePatternSelection].values; //data[extensionWidget.state.statePatternSelection];
 			var table = document.getElementById('show_pattern_table') as HTMLTableElement;
-			for (var i=0;i< values.length;i++){
+			Object.keys(values).forEach(function(key, i){
 				var row = table.insertRow(i);
 				var cell1 = row.insertCell(0);
-				var cell2 = row.insertCell(1); 
+				var cell2 = row.insertCell(1);
 				var t1 = document.createElement("label");
-				t1.innerHTML = values[i];
-				t1.id = "label"+i;
+				t1.id = "label"+ i;
+				t1.innerHTML = key;
 				var t2 = document.createElement("input");
-				t2.id = "txtbox"+i;
-				t2.placeholder = values[i];
+				t2.id = "txtbox"+ i;
+				t2.placeholder = key;
+				t2.autocomplete = "off";
 				cell1.appendChild(t1);
 				cell2.appendChild(t2);
-				if(btnExtension[i]==1){
+				if(values[key].extension==1){
 					var cell3 = row.insertCell(2);
 					var t3 = document.createElement("button");
 					t3.innerHTML = "+";
-					t3.id = "btn"+ values[i];
+					t3.id = "btn"+ key;
 					cell3.appendChild(t3);
-					t3.addEventListener('click', (event) => {
-						this.buttonClick(table, ( event.target as Element).id, counter +1);
-						counter = counter + 1;
-					});	
+					//t3.addEventListener('click', (event) => {
+					//	this.buttonClick(table, ( event.target as Element).id);
+					//});	
 				}
-			}
+				var classes = values[key]["classes"];
+				if (classes != null){
+					Object.keys(classes).forEach(function(key){
+						var row = table.insertRow(table.rows.length);
+						var cell1 = row.insertCell(0);
+						var cell2 = row.insertCell(1); 
+						var t1 = document.createElement("label");
+						t1.innerHTML = key;
+						t1.id = "label"+table.rows.length;
+						var t2 = document.createElement("input");
+						t2.id = "txtbox"+table.rows.length;
+						t2.placeholder = key;
+						cell1.appendChild(t1);
+						cell2.appendChild(t2);
+
+			});
+		}
+		});
 			
 
 		}else if (extensionWidget.state.stateURLproject=='Project URL' || extensionWidget.state.stateURLproject==""){
@@ -405,29 +164,15 @@ export class extensionWidget extends ReactWidget {
 		
 	}
 	//when button is clicked adds one label and one input of the specific class that the user wants to insert one more 
-	buttonClick (table: HTMLTableElement, value: string, counter: integer): void {
-		var size = table.rows.length;	
-		var row = table.insertRow(size);
-		var cell1 = row.insertCell(0);
-		var cell2 = row.insertCell(1);
-		var t1 = document.createElement("label");
-		t1.id = "label"+(size);
-		value = this.updateLabelValue(value.substr(3,), counter);
-		t1.innerHTML = value;
-		var t2 = document.createElement("input");
-		t2.id = "txtbox"+(size);
-		t2.placeholder = value;
-		t2.autocomplete = "off";
-		cell1.appendChild(t1);
-		cell2.appendChild(t2);
+	buttonClick (table: HTMLTableElement, value: string): void {
+	
 	}
 
-	updateLabelValue(string : string, counter: integer): string {
+	updateLabelValue(string : string, counter: number): string {
 		let lastChar = string.slice(-1);
 		string = string.replace(lastChar, counter.toString());
 		return string;
 	}
-	
 }	
 
 
