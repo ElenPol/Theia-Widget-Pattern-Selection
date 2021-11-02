51f3ed99e3b332270ea2bc82b6f8cc0fd1acc587
Elen Pol <elen_pol@hotmail.com>
2021-11-02 13:12:01 -0700

    List of file names in open folder

--- extension/src/node/hello-backend-service.ts
+++ extension/src/node/hello-backend-service.ts
@@ -1,7 +1,7 @@
 import { injectable, inject } from "inversify";
 import { HelloBackendService } from "../common/protocol";
 import {FileSearchService } from "@theia/file-search/lib/common/file-search-service";
-import * as fs from 'fs';
+//import * as fs from 'fs';
 
 @injectable()
 export class HelloBackendServiceImpl implements HelloBackendService {
@@ -10,8 +10,9 @@ export class HelloBackendServiceImpl implements HelloBackendService {
     protected readonly fileSearchService!:FileSearchService;
       
     static index = -1;
+    
 
-    async sayHelloTo(url: string, table: Array<string>): Promise<number> {
+    async sayHelloTo(url: string): Promise<string[]> {
         //string manipulation to get the right form of url string
         var lastL = url.indexOf("/#/");
         var rootUri = url.substr(lastL+3);
@@ -29,22 +30,11 @@ export class HelloBackendServiceImpl implements HelloBackendService {
         //search for every file name in textbox values
         //index=-1 if not found
         var res= await this.fileSearchService.find('',opts);
-		    res.forEach(function (str2){
-                var lastW = str2.lastIndexOf("/");
-                var file = str2.substr(lastW+1);
-                file = file.substr(0, file.indexOf("."));
-                for(var j = 0;j<table.length;j++) {
-                    if (table[j]==file){
-                        HelloBackendServiceImpl.index = table.indexOf(table[j]);
-                        console.log("IN" + HelloBackendServiceImpl.index);
-                        return new Promise<number>(resolve => resolve(HelloBackendServiceImpl.index));
-                    }
-                }
-            });
-            
-           console.log(fs.readFileSync('C:/Users/test/Downloads/src/src/Main.java','utf8'));
+                    
+        //fs
+        //console.log(fs.readFileSync('C:/Users/test/Downloads/src/src/Main.java','utf8'));
 
-        return new Promise<number>(resolve => resolve(HelloBackendServiceImpl.index));
+        return new Promise<string[]>(resolve => resolve(res));
 
 
     
